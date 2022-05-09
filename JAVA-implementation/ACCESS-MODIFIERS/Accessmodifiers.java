f74a94969000f872031ae8c37105ee9714cd9538
SriHarikavati18 <105206132+SriHarikavati18@users.noreply.github.com>
2022-05-09 08:16:11 -0700

    Add files via upload

---
+++ JAVA-implementation/ACCESS-MODIFIERS/Accessmodifiers.java
@@ -0,0 +1,29 @@
+package package1;
+
+public class Access1 {
+	//it has some data or information with useless because it caan only have the information but nothing to do
+	//four types of access modifiers
+	//default
+public  int hours=23;
+	public int minutes=12;
+	  public int gethours(){
+		  return hours;
+		  
+		  // 
+		  
+	  }
+
+}
+/* proctected  int hours=23;
+protected  int minutes=12;
+we can only extract the attributes by using extends keyword
+ 
+ */
+
+/*
+ private int hours=23;
+ private int minutes=12;
+ if we define the attributes with private we cannot get the attributes for same package and other package
+  we cant fetch the attributes
+ 
+*/
